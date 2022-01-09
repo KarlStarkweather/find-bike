10a98e34ab4f68fb1530fbd35dc504f9cef722dc
KarlStarkweather <stark13@usa.net>
2022-01-09 12:10:51 -0800

    marginal functionality of API call

--- src/index.js
+++ src/index.js
@@ -9,15 +9,21 @@ function clearFields() {
   $('.results').text("");
 }
 
+// function getDescriptions(bikes) {
+//   bikes.array.forEach(element => {
+//     let d = d & element & '/n';
+//   });
+// }
+
 $(document).ready(function() {
-  $('#findBike#search').submit(function(event) {
+  $('#findBike').submit(function(event) {
     event.preventDefault();
     let distance = $('#searchDistance').val();
     clearFields();
     BikeService.findBike(distance)
       .then(function(response) {
         if (response.bikes) {
-          $('#results').text(`${response.bikes.length} bikes found in this area, the first description is: ${response.bikes[0][description]}`);
+          $('#results').text(`${response.bikes.length} bikes found in this area, the first description is: ${response.bikes[2]["description"]}`);
         } else {
           $('#errors').text(`There was an error: ${response.message}`);
         }
@@ -25,23 +31,3 @@ $(document).ready(function() {
   });
 });
 
-
-// WeatherService.getWeather(city)
-//       .then(function(response) {
-//         getElements(response);
-//       });
-
-// }
-// $(document).ready(function() {
-//   $('#findBike').click(function() {
-//     let city = $('#location').val();
-//     $('#searchDistance').val("");
-//     clearFields();
-// // function getElements(response) {
-// //   if (response.main) {
-// //     $('.results').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-// //     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-// //   } else {
-// //     $('.showErrors').text(`There was an error: ${response.message}`);
-// //   }
-// // }

ab863341da98c493753281ef15c1496a0c87ebe9
KarlStarkweather <stark13@usa.net>
2022-01-09 11:20:26 -0800

    initial index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,46 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
+import BikeService from '../src/bike-service';
+
+function clearFields() {
+  $('#searchDistance').val("");
+  $('.results').text("");
+}
 
 $(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
+  $('#findBike#search').submit(function(event) {
     event.preventDefault();
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3)
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
+    let distance = $('#searchDistance').val();
+    clearFields();
+    BikeService.findBike(distance)
+      .then(function(response) {
+        if (response.bikes) {
+          $('#results').text(`${response.bikes.length} bikes found in this area, the first description is: ${response.bikes[0][description]}`);
+        } else {
+          $('#errors').text(`There was an error: ${response.message}`);
+        }
+      })
   });
 });
+
+
+// WeatherService.getWeather(city)
+//       .then(function(response) {
+//         getElements(response);
+//       });
+
+// }
+// $(document).ready(function() {
+//   $('#findBike').click(function() {
+//     let city = $('#location').val();
+//     $('#searchDistance').val("");
+//     clearFields();
+// // function getElements(response) {
+// //   if (response.main) {
+// //     $('.results').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+// //     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+// //   } else {
+// //     $('.showErrors').text(`There was an error: ${response.message}`);
+// //   }
+// // }
